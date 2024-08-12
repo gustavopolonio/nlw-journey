6c31127a5599eaf6bd66d3622b8418ed916c5413
Gustavo <gustavopolonio1@gmail.com>
2024-08-12 16:28:44 -0300

    feat: create error page

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,15 +1,39 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { CreateTripPage } from './pages/create-trip';
 import { TripDetailsPage } from './pages/trip-details';
+import { ErrorPage } from './pages/error';
+import { DestinationAndDateHeader } from './pages/trip-details/destination-and-date-header';
+import { Activities } from './pages/trip-details/activities';
+import { Guests } from './pages/trip-details/guests';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <CreateTripPage />,
+    errorElement: <ErrorPage />,
   },
   {
     path: '/trips/:tripId',
     element: <TripDetailsPage />,
+    loader: async ({ params, ...args }) => {
+      const loaderArgs = {
+        params: {
+          tripId: params.tripId,
+        },
+        ...args,
+      };
+
+      const trip = await DestinationAndDateHeader.loader(loaderArgs);
+      const activities = await Activities.loader(loaderArgs);
+      const participants = await Guests.loader(loaderArgs);
+
+      return {
+        ...trip,
+        ...activities,
+        ...participants,
+      };
+    },
+    errorElement: <ErrorPage />,
   },
 ]);
 
